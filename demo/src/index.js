4dd46dca02c71eb1f5b4cc2ebd5bd7542de285d2
CARLOS\User <carloscba1@gmail.com>
2018-03-01 13:06:58 -0300

    - Ejemplo completo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,19 +4,36 @@ import {render} from 'react-dom'
 import Translate from '../../src'
 
 class Demo extends Component {
+  
+  constructor(){
+    super()
+    this.state = {
+        locale : 'en'
+    }
+    
+  }
+
+  switchLanguage = () => {
+      this.setState({
+          locale : (this.state.locale === 'en') ? 'es' : 'en'
+      })
+  }
+
   render() {
     return <div>
-      <Translate locale="en">
+      <button onClick={ this.switchLanguage }>Switch Language ({ this.state.locale })</button>
+      
+      <Translate locale={ this.state.locale }>
         <h2 ref="es">Hola Mundo</h2>
         <h2 ref="en">Hello World</h2>
       </Translate>      
 
-      <Translate locale="en">
+      <Translate locale={ this.state.locale }>
         <p ref="es">Español</p>
         <p ref="en">English</p>
       </Translate>
 
-      <Translate locale="en">
+      <Translate locale={ this.state.locale }>
         <div ref="es">
           <p><strong>Lorem Ipsum</strong> es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
         </div>
